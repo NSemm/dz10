abc6bba33c89b513396c16c7763ef1c88cebc7ce
NickSemerenko <nickolay.sem@gmail.com>
2020-10-27 20:24:00 +0200

    add method overr equals

---
+++ src/Humans/Human.java
@@ -0,0 +1,45 @@
+package Humans;
+
+public class Human {
+    private String name;
+    private String surname;
+    private String age;
+
+    public Human(String name, String surname, String age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj) == null) return false;
+        if (!(obj instanceof Human)) return false;
+        Human human = (Human) obj;
+        return human.name == name && human.surname == surname && human.age == age;
+    }
+}
